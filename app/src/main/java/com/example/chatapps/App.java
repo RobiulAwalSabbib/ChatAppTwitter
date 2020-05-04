23214ee1ffa902a7136282dc22e397bb762d6c2a
robiulawal <engg.robiulawal@gmail.com>
2020-05-04 23:24:38 +0600

    initial commit

---
+++ app/src/main/java/com/example/chatapps/App.java
@@ -0,0 +1,53 @@
+package com.example.chatapps;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import static com.parse.Parse.getApplicationContext;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123cfbgcfbcvbn");
+        object.put("myString", "robiul");
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
+
+
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+
+
+}
